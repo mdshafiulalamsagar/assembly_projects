2bf5e470c105b938d0b07d99448599d46f3cd00d
MD Shafiul Alam Sagar <m.shafiulalamsagar@gmail.com>
2025-05-13 11:40:16 +0600

    Update unhealthytype.asm

--- project_two/unhealthytype.asm
+++ project_two/unhealthytype.asm
@@ -1,35 +1,26 @@
-.model small
-.stack 100h
+org 100h   
 
-.data
-    warning_msg db 0Dh, 0Ah, 'Warning: Your typing is unhealthy!', 0Dh, 0Ah, '$'
-    counter db 0
+jmp start   
 
-.code
-org 100h
+
+warning_msg db 0Dh, 0Ah, 'Warning: Your typing is unhealthy!', 0Dh, 0Ah, '$'
+counter     db 0
+
 
 start:
-    ; Jump over data declarations
-    jmp main
-
-main:
-    ; Set data segment
-    mov ax, @data
+    mov ax, 0B800h     
     mov ds, ax
 
+   
+
 check_key:
-    ; Wait for key press
     mov ah, 00h
-    int 16h
-    ; AL = ASCII, AH = Scan code
+    int 16h            
+    mov bl, ah       
 
-    mov bl, ah  ; Save scan code for later
-
-    ; Check if key is '5'
     cmp al, '5'
     je increment_counter
 
-    ; Reset counter for non-'5' keys
     mov counter, 0
     jmp check_exit
 
@@ -38,20 +29,15 @@ increment_counter:
     cmp counter, 5
     jl check_exit
 
-    ; Display warning message
     mov ah, 09h
     mov dx, offset warning_msg
     int 21h
 
-    ; Reset counter
     mov counter, 0
 
 check_exit:
-    cmp bl, 01h      ; ESC key scan code
+    cmp bl, 01h         
     jne check_key
 
-    ; Terminate program
     mov ax, 4C00h
     int 21h
-
-end start
