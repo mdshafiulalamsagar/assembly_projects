9e5ae0220c9732497330927517060a097c986d5a
MD Shafiul Alam Sagar <m.shafiulalamsagar@gmail.com>
2025-05-13 11:59:21 +0600

    Update unhealthytype.asm

--- project_two/unhealthytype.asm
+++ project_two/unhealthytype.asm
@@ -1,43 +1,36 @@
-org 100h   
+org 100h           
 
-jmp start   
+jmp start          
 
 
 warning_msg db 0Dh, 0Ah, 'Warning: Your typing is unhealthy!', 0Dh, 0Ah, '$'
-counter     db 0
+counter      db 0
 
 
 start:
-    mov ax, 0B800h     
-    mov ds, ax
 
-   
 
 check_key:
     mov ah, 00h
-    int 16h            
+    int 16h        
     mov bl, ah       
 
-    cmp al, '5'
-    je increment_counter
-
-    mov counter, 0
-    jmp check_exit
-
-increment_counter:
     inc counter
     cmp counter, 5
-    jl check_exit
+    jl check_exit    
 
+    ; Print message
     mov ah, 09h
     mov dx, offset warning_msg
     int 21h
 
+    ; Reset counter
     mov counter, 0
 
 check_exit:
-    cmp bl, 01h         
-    jne check_key
+    cmp bl, 01h       
+    jne check_key     
 
+    ; Exit program
     mov ax, 4C00h
     int 21h
